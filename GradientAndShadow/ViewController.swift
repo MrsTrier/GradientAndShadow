10e8217f73006de5324f2b9ac12ebb86d327a200
Cheremina Daria <d.cheremina@gmail.com>
2024-11-01 08:49:44 +0100

    Initial Commit

---
+++ GradientAndShadow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradientAndShadow
+//
+//  Created by Daria Cheremina on 01/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
